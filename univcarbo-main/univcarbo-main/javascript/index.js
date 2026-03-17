2f032feafc8733beb40d2b68e2bdc42889f0a0ae
Iamarnirich <168168574+Iamarnirich@users.noreply.github.com>
2026-03-17 23:00:21 +0100

    Delete univcarbo-main/univcarbo-main directory

--- univcarbo-main/univcarbo-main/javascript/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.getElementById('logo-screen').style.display = 'none';
-        document.getElementById('main-content').classList.add('show');
-        document.body.style.overflow = 'auto'; 
-    },1000); 
-});
